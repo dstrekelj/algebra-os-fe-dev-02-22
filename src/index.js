3502b1c1a02fe439da3690ef5ffcedab00b6d079
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-24 20:31:43 +0200

    Add CSS reset dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./reset.css";
+import "the-new-css-reset/css/reset.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
