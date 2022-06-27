04a49f999fb825012c011b0715b6f0eb40972bf7
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-27 20:29:26 +0200

    Add form error and message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import "./App.css";
 import { InputElement } from "./components/InputElement";
 import Button from "./components/Button";
 
+function FormError(props) {
+  if (!props.visible) return null;
+
+  return <div className="form-error">Failed to sign in!</div>;
+}
+
+function FormMessage(props) {
+  if (!props.visible) return null;
+
+  return <div className="form-message">Successfully signed in!</div>;
+}
+
 function App() {
+  const isSignedIn = true;
+
   return (
     <form className="form">
       <div className="form-field">
@@ -15,6 +29,8 @@ function App() {
         <Button type="button">Sign in</Button>
         <Button type="reset">Reset</Button>
       </div>
+      <FormError visible={!isSignedIn} />
+      <FormMessage visible={isSignedIn} />
     </form>
   );
 }
