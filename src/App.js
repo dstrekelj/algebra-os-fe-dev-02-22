2d47e2b81a7f1c09d7e8c3d11a3b89a3422c53d5
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-29 20:56:03 +0200

    Hide form on sign in

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       <Counter initialValue={0} />
       <Counter initialValue={100} />
       {isSignedIn && <User src="/avatar.svg" username={user.username} />}
-      <form className="form" onSubmit={handleSubmit}>
+      {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
           <InputElement name="username" label="Username" type="text" onChange={handleChange} />
         </div>
@@ -52,7 +52,7 @@ function App() {
         </div>
         <FormError visible={showErrorMessage} />
         <FormMessage visible={showFormMessage} />
-      </form>
+      </form>}
     </div>
   );
 }
