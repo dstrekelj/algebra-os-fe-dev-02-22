9708e90b5328f873e86f2ddad6e5e146b84b5092
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-30 18:17:39 +0200

    Remove password input

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import { User } from "./components/User";
 import { useState } from "react";
 
 function App() {
-  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ username: '' });
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (formState.password === 'password') {
+    if (formState.username === '') {
+      setError(true);
+    } else {
       setUser(formState);
       setError(null);
-    } else {
-      setError(true);
     }
   }
 
@@ -45,9 +45,6 @@ function App() {
       {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
           <InputElement name="username" label="Username" type="text" onChange={handleChange} />
-        </div>
-        <div className="form-field">
-          <InputElement name="password" label="Password" type="password" onChange={handleChange} />
         </div>
         <div className="form-field">
           <Button type="submit">Sign in</Button>
