9f90b900626db5779428561dd70589332c120752
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-30 19:08:29 +0200

    Add message form

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -1,11 +1,39 @@
 import Button from "../components/Button";
+import { InputElement } from "../components/InputElement";
 import { User } from "../components/User";
+import { useState } from "react";
 
 export function ChatRoomPage(props) {
+  const [formState, setFormState] = useState({ message: '' });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.message !== '') {
+      // Send message
+      console.log(formState);
+    }
+  }
+  
+  const handleChange = (event) => {
+    setFormState((state) => ({
+          ...state,
+          [event.target.name]: event.target.value,
+    }));
+  }
+
   return (
     <div>
       <Button type="button" onClick={props.onSignOut}>Sign out</Button>
       <User src="/avatar.svg" username={props.user.username} />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <InputElement name="message" label="Message" type="text" onChange={handleChange} />
+        </div>
+        <div className="form-field">
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
     </div>
   );
 }
