6cf4a9aefe2f264a3c59a41be42541fc90d1226f
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-06-30 18:44:14 +0200

    Add sign in page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import "./App.css";
-import { InputElement } from "./components/InputElement";
 import Button from "./components/Button";
-import FormError from "./components/FormError";
-import FormMessage from "./components/FormMessage";
 import { User } from "./components/User";
 import { useState } from "react";
+import { SignInPage } from "./pages/SignInPage";
 
 function App() {
-  const [formState, setFormState] = useState({ username: '' });
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (formState.username === '') {
-      setError(true);
-    } else {
-      setUser(formState);
-      setError(null);
-      setFormState({ username: '' });
-    }
-  }
-
-  const handleChange = (event) => {
-    setFormState((state) => ({
-          ...state,
-          [event.target.name]: event.target.value,
-    }));
+  const handleSignIn = (user) => {
+    setUser(user);
   }
 
   const signOut = () => {
@@ -36,23 +18,12 @@ function App() {
   }
 
   const isSignedIn = user !== null;
-  const showErrorMessage = error !== null;
-  const showFormMessage = user !== null;
 
   return (
     <div>
       {isSignedIn && <Button type="button" onClick={signOut}>Sign out</Button>}
       {isSignedIn && <User src="/avatar.svg" username={user.username} />}
-      {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
-        <div className="form-field">
-          <InputElement name="username" label="Username" type="text" onChange={handleChange} />
-        </div>
-        <div className="form-field">
-          <Button type="submit">Sign in</Button>
-        </div>
-        <FormError visible={showErrorMessage} />
-        <FormMessage visible={showFormMessage} />
-      </form>}
+      {!isSignedIn && <SignInPage onSignIn={handleSignIn} />}
     </div>
   );
 }
